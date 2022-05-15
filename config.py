f253adb82afd984025a9789c956ff9da5b364523
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-15 23:17:41 +0300

    ft: Heroku deployment attempt

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgres://uvwvmkiqhgfubf:abe513190f4f7f07835a812186460cc53c065e6e4ace40ddaf28182a21fc02af@ec2-54-86-224-85.compute-1.amazonaws.com:5432/d7i76dml2jcpeh'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://uvwvmkiqhgfubf:abe513190f4f7f07835a812186460cc53c065e6e4ace40ddaf28182a21fc02af@ec2-54-86-224-85.compute-1.amazonaws.com:5432/d7i76dml2jcpeh'
     conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI, sslmode='require')
 class DevConfig(Config):
     '''

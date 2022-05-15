402e05c6e370bb5702cb6040dda0cb27755d6425
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-15 15:04:26 +0300

    ft: Model classes created for our users authentification and to store blogs

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,31 @@
 from flask import Flask
-from config import DevConfig
+from config import DevConfig,config_options
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 # Initializing application
-
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
-app = Flask(__name__)
+def create_app(config_name):
+# Initializing application
+    app = Flask(__name__)
 
-bootstrap.init_app(app)
 
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
-app.config.from_object(DevConfig)
-app.config['SECRET_KEY'] = '1234'
+    app.config.from_object(config_options[config_name])
+    app.config.from_object(DevConfig)
+    app.config['SECRET_KEY'] = '1234'
 
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
